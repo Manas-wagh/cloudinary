a0677487663c48aa367f9ca73411e35af58df197
manas <manasw649@gmail.com>
2023-03-31 02:01:36 +0530

    cloudinary

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import CloudinaryUploadWidget from "./Cloud";
+import './App.css';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h3>Cloudinary Upload Widget Example</h3>
+      <CloudinaryUploadWidget>
+      
+      {/* <img id="uploadedimage" src=""></img> */}
+     
+      </CloudinaryUploadWidget>
+    </div>
+  );
+}
+
